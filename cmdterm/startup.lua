06eb7a240b52db681ba4f6b27e0337f7e9cea6d6
Duhsten <dustinkosborne@gmail.com>
2022-03-28 18:33:33 -0400

    Updated

--- cmdterm/startup.lua
+++ cmdterm/startup.lua
@@ -1,8 +1,11 @@
 local modem = peripheral.wrap("back")
+modem.open(2)
 
 function commandLine()
     local input = read("cmd: ")
     modem.transmit(1,2, input)
+    event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")
+    print(message)
 end
 
 while true do
