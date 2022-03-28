f6f084ab80ec801123a74a45552cbbad059d0327
Duhsten <dustinkosborne@gmail.com>
2022-03-28 18:38:13 -0400

    Updated

--- cmdterm/startup.lua
+++ cmdterm/startup.lua
@@ -5,8 +5,13 @@ function commandLine()
     print("cmd")
     local input = read()
     modem.transmit(1,2, input)
+    print("")
+    term.setTextColor( colors.cyan )
     event, side, frequency, replyFrequency, message, distance = os.pullEvent("modem_message")
+    
     print(message)
+    term.setTextColor( colors.white )
+    print("")
 end
 
 while true do
